77a1d6b2f79625feb8001cde976c0303c00ea06f
iMaro40 <maro.santiago@yahoo.com>
2019-04-21 23:45:52 +0800

    Updates

---
+++ precache-manifest.397471dd9d7acca086900be48e3d54a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dbf06d88ca65242de6f0",
+    "url": "/yolo/static/js/runtime~main.b4584dd2.js"
+  },
+  {
+    "revision": "1f566d42b706b2d6ff6e",
+    "url": "/yolo/static/js/main.71ad80f0.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/yolo/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "1f566d42b706b2d6ff6e",
+    "url": "/yolo/static/css/main.ce741728.chunk.css"
+  },
+  {
+    "revision": "0558b41e0b71fd20c4ec232b440d9107",
+    "url": "/yolo/index.html"
+  }
+];
